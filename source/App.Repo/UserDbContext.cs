f2d53b7d112257f130b26ab87f48c21d26c8af4b
GuilhermeGS <ggsartori@inf.ufrgs.br>
2019-11-21 18:07:04 -0300

    ADDED: User Db Context Defined. EntityFrameworkCore installed

---
+++ source/App.Repo/UserDbContext.cs
@@ -0,0 +1,21 @@
+﻿//necessary package to use in-memory SQL Server database. Version 1.1.1, installed using Visual Studio NuGet
+using App.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Repo
+{
+    class UserDbContext:DbContext
+    {
+        //constructor (creates connection with the entityframeworkcore (in-memory Db) 
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+        //with this command, the User Table is already available in the database
+        //code first, not db first
+
+        //Generate for the User class the Users table
+        public DbSet<User> Users { get; set; }
+
+    }
+}
